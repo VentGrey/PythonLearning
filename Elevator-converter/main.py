308ad06e1c0ca2ad1f232a777910871bf70253d3
VentGrey <omarpurataf@gmail.com>
2019-03-20 20:20:14 -0600

    Added elevator floor converter

---
+++ Elevator-converter/main.py
@@ -0,0 +1,6 @@
+# Este programa convierte pisos de elevadores  europeos a pisos de elevadores
+# estadounidenses
+
+Pisoe = input('Ingrese el piso del elevador Europeo')
+Pisous = int(Pisoe) + 1
+print("El piso en USA es:", Pisous)
