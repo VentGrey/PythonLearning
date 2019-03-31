67a20c46bc0f8b3594e66d2d6b8fb3b9a9f7cbe2
VentGrey <omarpurataf@gmail.com>
2019-03-31 14:02:03 -0600

    Extended data types in TS

---
+++ TypeScript/data-types/main.ts
@@ -0,0 +1,19 @@
+// El programa anterior fue una prueba muy a lo JavaScript
+// afortunadamente TypeScript posee una ventaja y es que tenemos varios tipos de
+// datos: Números, Strings, Structs, Booleanos y demás.
+
+
+// BOOLEANOS
+let booleano: boolean = true; // booleanos: true / false
+let booleano1: boolean = false;
+
+// NUMEROS
+let entero: number = 6;
+let hexadecimal: number = 0xf00d;
+let binario: number = 0b1010;
+let octal: number = 0o744;
+
+
+// CADENAS
+let color: string = "Rojo";
+color = 'azul';
