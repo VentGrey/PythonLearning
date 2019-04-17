ed3246c3cd47de616e2cd3ce8bf63bed21e7de59
VentGrey <omarpurataf@gmail.com>
2019-04-17 00:43:56 -0500

    Finished financiamiento

---
+++ Python3/Practices/financiamiento.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+MON = float(input("Ingrese el monto total del vehículo "))
+print("El enganche a pagar es de: ",MON * 0.35)
+print("Las mensualidades a pagar son de: " MON / 18)
