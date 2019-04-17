1f2dd2463a4274e374ce3814b2a1a817c24ea48b
VentGrey <omarpurataf@gmail.com>
2019-04-17 01:03:09 -0500

    Finished esfera.py

---
+++ Python3/Practices/esfera.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+PI: float = 3.1415
+
+RAD = float(input("Ingrese el radio a evaluar"))
+print("El área de la esfera es de: ", 4 * PI * (RAD ** 2))
+print("El volúmen de la esfera es de: ", (4/3) * PI * (RAD ** 3))
