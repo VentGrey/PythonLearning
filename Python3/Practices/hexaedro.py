42fb5d734ed8985221e8eccee33f912f868ba4f6
VentGrey <omarpurataf@gmail.com>
2019-04-17 01:05:40 -0500

    Added hexaedro.py

---
+++ Python3/Practices/hexaedro.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+L = float(input("Ingrese el lado del hexaedro: "))
+print("El área de la base del hexaedro es de: ", L**2)
+print("El área lateral es de: ", 4 * (L**2))
+print("El área total es de: ", 6 * (L**2))
+print("El volumen es de: ", L**3)
