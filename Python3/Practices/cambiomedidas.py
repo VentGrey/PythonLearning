d4121a53c40c9ca6e23a563b535221485efc8c2d
VentGrey <omarpurataf@gmail.com>
2019-04-16 21:47:00 -0500

    missing space

--- Python3/Practices/cambiomedidas.py
+++ Python3/Practices/cambiomedidas.py
@@ -2,5 +2,5 @@
 
 hectarea = 0.404686
 
-acres = float(input("Ingrese la cantidad de acres que desea convertir"))
+acres = float(input("Ingrese la cantidad de acres que desea convertir "))
 print("Usted posee: ", acres * hectarea, " hectáreas")
