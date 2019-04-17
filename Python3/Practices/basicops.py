4e11f345a2d6920070dccf20a275d5f483724ee6
VentGrey <omarpurataf@gmail.com>
2019-04-16 21:40:27 -0500

    Added spaces

--- Python3/Practices/basicops.py
+++ Python3/Practices/basicops.py
@@ -5,8 +5,8 @@ Escriba un programa que dado como datos dos números reales, calcule la suma,
 resta, multiplicación y división de dichos números.
 """
 
-num1 = float(input("Ingrese el primer número a tratar"))
-num2 = float(input("Ingrese el segundo número a tratar"))
+num1 = float(input("Ingrese el primer número a tratar: "))
+num2 = float(input("Ingrese el segundo número a tratar: "))
 
 print("La suma de los números es: ", num1 + num2)
 print("La diferencia entre números es: ", num1 - num2)
--- Python3/Practices/circle.py
+++ Python3/Practices/circle.py
@@ -2,6 +2,6 @@
 
 PI = 3.1415
 
-rad = float(input("Ingrese el radio del círculo"))
+rad = float(input("Ingrese el radio del círculo: "))
 print("El área del círculo es: ", PI * (rad **2))
 print("La circunferencia es: ", 2 * PI * rad)
