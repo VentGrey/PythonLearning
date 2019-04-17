ea8143d8627f23bb40141b14d4d84dc6a20f4dc2
VentGrey <omarpurataf@gmail.com>
2019-04-16 21:56:20 -0500

    Added interes bancario

---
+++ Python3/Practices/interesbancario.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+MD = float(input("Ingrese el monto de dinero que depositará "))
+TASA = float(input("Ingrese la tasa de interés mensual\
+que le cobrarán (en decimales) "))
+
+print("Al final de lmes usted tendrá un total de: ", MD + (MD / (TASA * 10)))
