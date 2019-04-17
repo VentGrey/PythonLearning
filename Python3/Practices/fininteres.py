86a51a6c5787cb73b18e94c029858d561999c6a5
VentGrey <omarpurataf@gmail.com>
2019-04-17 00:47:45 -0500

    Added fininteres.py

---
+++ Python3/Practices/fininteres.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+MON = float(input("Ingrese el monto total del vehículo: "))
+print("El enganche del vehículo es de: ", MON *= 0.35)
+print("Las mensualidades son de: ", 0.12 *= (MON / 36))
