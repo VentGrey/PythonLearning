14bb6de96b7910a069b3d2126fe959e2f4fa6cdc
VentGrey <omarpurataf@gmail.com>
2019-04-16 21:29:27 -0500

    Se añadió el modulo de operaciones básicas

---
+++ Python3/Practices/basicops.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+"""
+Escriba un programa que dado como datos dos números reales, calcule la suma,
+resta, multiplicación y división de dichos números.
+"""
+
+num1 = float(input("Ingrese el primer número a tratar"))
+num2 = float(input("Ingrese el segundo número a tratar"))
+
+print("La suma de los números es: ", num1 + num2)
+print("La diferencia entre números es: ", num1 - num2)
+print("El producto entre números es: ", num1 * num2)
+print("La división de los números es: ", num1 / num2)
