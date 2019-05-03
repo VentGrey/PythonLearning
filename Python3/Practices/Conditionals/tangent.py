bda189c088464c6e08357bb1e8bf636559349310
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-05-02 20:18:58 -0500

    Added python file

---
+++ Python3/Practices/Conditionals/tangent.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import math
+
+theta = float(input("Ingrese el valor de theta: "))
+
+tan = (math.sin(theta) / math.cos(theta))
+if math.sin(theta) == 0:
+    print("No se pueden hacer divisiones entre cero")
+
+
+cot = (math.cos(theta) / math.sin(theta))
+
+if
